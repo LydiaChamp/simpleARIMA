cdc477861b71004bdb01175d4dc1b6a746c4ae0b
ECDCNET\lchampezou <lhabezou@gmail.com>
2023-11-07 13:11:17 +0100

    no message

--- code/01_SetUp.R
+++ code/01_SetUp.R
@@ -4,12 +4,14 @@ library(dplyr)
 library(tidyverse)
 library(forecast)
 library(countrycode)
+library(RCurl)
 
 # ---- Load Data ----
 #data <- read_csv("data/data.csv")
 #View(data)
-data_cases <- read_csv("data/truth_ECDC-Incident Cases.csv")
-data_deaths <- read_csv("data/truth_ECDC-Incident Deaths.csv")
+
+data_cases <- read.csv("https://raw.githubusercontent.com/european-modelling-hubs/covid19-forecast-hub-europe/main/data-truth/ECDC/truncated_ECDC-Incident%20Cases.csv")
+data_deaths <- read_csv("https://raw.githubusercontent.com/european-modelling-hubs/covid19-forecast-hub-europe/main/data-truth/ECDC/truncated_ECDC-Incident%20Deaths.csv")
 
 #data_hosps <- read_csv("data/weekly-hospital-admissions-covid.csv")
 #View(data_hosps)
